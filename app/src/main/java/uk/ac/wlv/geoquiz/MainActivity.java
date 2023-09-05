737a090d7952a612913f364b9474b567085500b2
KiwiFarah <warnakulasuriyafarah@gmail.com>
2023-09-05 13:01:16 +0530

    Wiring Up Widgets

--- app/src/main/java/uk/ac/wlv/geoquiz/MainActivity.java
+++ app/src/main/java/uk/ac/wlv/geoquiz/MainActivity.java
@@ -1,10 +1,14 @@
 package uk.ac.wlv.geoquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private TextView mQuestionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
