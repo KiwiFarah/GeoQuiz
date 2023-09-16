73e35ed2993f93a545a2c599a05dbb9ce37ed68b
KiwiFarah <warnakulasuriyafarah@gmail.com>
2023-09-17 00:30:40 +0530

    Passing data between activities

--- app/src/main/java/uk/ac/wlv/geoquiz/CheatActivity.java
+++ app/src/main/java/uk/ac/wlv/geoquiz/CheatActivity.java
@@ -1,10 +1,19 @@
 package uk.ac.wlv.geoquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 
 import android.os.Bundle;
 
 public class CheatActivity extends AppCompatActivity {
+    private static final String EXTRA_ANSWER_IS_TRUE = "uk.ac.wlv.geoquiz.answer is true";
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue){
+        Intent i = new Intent(packageContext, CheatActivity.class);
+        i.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return i;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
