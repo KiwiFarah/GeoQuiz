d58e33fab7f3299f74e8214db14a0ecf04cca1c5
KiwiFarah <warnakulasuriyafarah@gmail.com>
2023-09-17 00:12:35 +0530

    Add cheat button

---
+++ app/src/main/java/uk/ac/wlv/geoquiz/CheatActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.wlv.geoquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
